692334f73462480948b11c500a646f3e249bb20e
Ambaza-Marcelo <84043779+Ambaza-Marcelo@users.noreply.github.com>
2021-10-30 03:31:20 +0200

    chart 1

--- app/Http/Controllers/ChartController.php
+++ app/Http/Controllers/ChartController.php
@@ -55,27 +55,7 @@ public function index()
     $chart1 = new LaravelChart($chart_options);
 
 
-    $chart_options = [
-        'chart_title' => 'Users by names',
-        'report_type' => 'group_by_string',
-        'model' => 'App\Models\User',
-        'group_by_field' => 'name',
-        'chart_type' => 'pie',
-        'filter_field' => 'created_at',
-        'filter_period' => 'month', // show users only registered this month
-    ];
-
-    $chart2 = new LaravelChart($chart_options);
-
-    $chart_options = [
-        'chart_title' => 'Transactions by dates',
-        'report_type' => 'group_by_date',
-        'model' => 'App\Models\Transaction',
-        'group_by_field' => 'transaction_date',
-        'group_by_period' => 'day',
-        'aggregate_function' => 'sum',
-        'aggregate_field' => 'amount',
-        'chart_type' => 'line',
+    
     ];
 
     $chart3 = new LaravelChart($chart_options);
