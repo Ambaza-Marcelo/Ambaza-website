5660450c5fe055fa1387e6469087eb3d08e184a6
Ambaza-Marcelo <84043779+Ambaza-Marcelo@users.noreply.github.com>
2021-10-30 03:26:04 +0200

    post model

--- app/Post.php
+++ app/Post.php
@@ -2,14 +2,3 @@
 
 namespace App;
 
-use Illuminate\Database\Eloquent\Model;
-
-class Post extends Model
-{
-    //
-    protected $fillable =[
-    	'title',
-    	'description',
-    	'image'
-    ];
-}
