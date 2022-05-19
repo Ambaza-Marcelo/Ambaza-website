b1526c00c1dc7ed8eb23e270c8f18378a30d6d98
Ambaza-Marcelo <marcelin@net-telecom.net>
2022-05-19 14:31:15 +0200

    header icon

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -26,16 +26,6 @@
    <body>
     <!-- Preloader Start -->
     <!-- ambaza marcellin
-    <div id="preloader-active">
-        <div class="preloader d-flex align-items-center justify-content-center">
-            <div class="preloader-inner position-relative">
-                <div class="preloader-circle"></div>
-                <div class="preloader-img pere-text">
-                    <img src="assets/img/logo/loder.jpg" alt="">
-                </div>
-            </div>
-        </div>
-    </div>
 -->
     <!-- Preloader Start -->
     <header>
